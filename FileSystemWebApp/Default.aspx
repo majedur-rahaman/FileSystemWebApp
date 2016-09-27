fc71180b2dea125995f6623b00709201cf3ed901
Majedur Rahaman <majed.cseru@gmail.com>
2016-09-27 20:56:36 +0600

    File create, read, write and append

---
+++ FileSystemWebApp/Default.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FileSystemWebApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>File System</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <div>
+                <div>File name with path:</div>
+                <div>
+                    <asp:TextBox ID="FilePathId" runat="server"></asp:TextBox>
+                    <asp:Label ID="ErrorLabel" ForeColor="Red" runat="server"></asp:Label>
+                    <span>
+                        <asp:Button runat="server" Text="Create" OnClick="CreateFile_Click" OnClientClick="return validate();"/>
+                    </span>
+
+                </div>
+            </div>
+            <div>
+                <div>File Content:
+                    <asp:Label ID="ErrorPermission" ForeColor="Red" runat="server"></asp:Label></div>
+                <div>
+                    <asp:TextBox ID="ContentTextArea" TextMode="multiline" Columns="50" Rows="5" runat="server" /></div>
+                <div>
+                    <asp:RadioButtonList ID="PermissionId" AutoPostBack="true" runat="server" OnSelectedIndexChanged="RadioPermissionChange_CheckedChanged">
+                        <asp:ListItem Value="1">Read</asp:ListItem>
+                        <asp:ListItem Value="2">Write</asp:ListItem>
+                        <asp:ListItem Value="3">Append</asp:ListItem>
+                    </asp:RadioButtonList>
+
+                </div>
+            </div>
+            <div>
+                <span>
+                    <asp:Button runat="server" Text="Read" OnClick="ReadFile_Click"  OnClientClick="return validate();"  />
+                </span>
+                <span>
+                    <asp:Button runat="server" Text="Write" OnClick="WriteOnFile_Click"  OnClientClick="return validate();" />
+                </span>
+                <span>
+                    <asp:Button runat="server" Text="Append" OnClientClick="return validate();" OnClick="AppendFile_Click" />
+                </span>
+                <span>
+                    <asp:DropDownList ID="ChangePermissionId" AutoPostBack="true" runat="server" OnSelectedIndexChanged="DropDownPermissionChange_CheckedChanged">
+                        <asp:ListItem Text="Change Permision" Value="0"></asp:ListItem>
+                        <asp:ListItem Text="Read" Value="1"></asp:ListItem>
+                        <asp:ListItem Text="Write" Value="2"></asp:ListItem>
+                        <asp:ListItem Text="Append" Value="3"></asp:ListItem>
+                    </asp:DropDownList>
+                </span>
+
+            </div>
+        </div>
+    </form>
+    <script type="text/javascript">
+        function validate() {
+            var filePath = document.getElementById("FilePathId").value;
+            var accessModeRadio = document.getElementById("PermissionId").value;
+            var accessModeDropDown = document.getElementById("ChangePermissionId").value;
+
+            if (filePath === "") {
+                document.getElementById("ErrorLabel").innerHTML = null;
+                alert("File path should not be null!");
+                return false;
+            }
+            if (accessModeRadio === "" || accessModeDropDown === "") {
+                alert("You Should define your file access mode!");
+                return false;
+            }
+            return true;
+        }
+    </script>
+</body>
+</html>
---
+++ FileSystemWebApp/Default.aspx.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace FileSystemWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private string FilePath { set; get; }
+        private string FileContent { set; get; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ErrorLabel.Text = "";
+        }
+
+        protected void CreateFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FilePath = FilePathId.Text;
+                var fileInfo = new FileInfo(FilePath);
+                if (File.Exists(FilePath))
+                {
+                    ErrorLabel.Text = "File Already Exist!";
+                }
+                else
+                {
+                    fileInfo.Directory?.Create();
+                    File.WriteAllText(fileInfo.FullName, FileContent);
+                    ErrorLabel.Text = "";
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception);
+            }
+        }
+
+        protected void ReadFile_Click(object sender, EventArgs e)
+        {
+            FilePath = FilePathId.Text;
+            if ((PermissionId.Text == "1" || ChangePermissionId.Text == "1") 
+                && !string.IsNullOrEmpty(FilePath))
+            {
+                try
+                {
+                    ContentTextArea.Text = File.ReadAllText(FilePath);
+                    ErrorPermission.Text = "";
+                }
+                catch (Exception exception)
+                {
+                    Debug.WriteLine(exception);
+                }
+                ContentTextArea.ReadOnly = true;
+            }
+            else
+            {
+                ErrorPermission.Text = "Required permission to read data from file!";
+            }
+
+        }
+
+        protected void WriteOnFile_Click(object sender, EventArgs e)
+        {
+            FilePath = FilePathId.Text;
+
+            if ((PermissionId.Text == "2" || ChangePermissionId.Text == "2") 
+                && !string.IsNullOrEmpty(FilePath))
+            {
+                
+                FileContent = ContentTextArea.Text;
+                var file = new FileInfo(FilePath);
+
+                try
+                {
+                    File.WriteAllText(file.FullName, FileContent);
+                    ErrorPermission.Text = "";
+                }
+                catch (Exception exception)
+                {
+                    Debug.WriteLine(exception);
+                }
+            }
+            else
+            {
+                ErrorPermission.Text = "Required permission to write data to file!";
+            }
+
+        }
+        protected void AppendFile_Click(object sender, EventArgs e)
+        {
+            FilePath = FilePathId.Text;
+
+            if ((PermissionId.Text == "3" || ChangePermissionId.Text == "3") 
+                && !string.IsNullOrEmpty(FilePath))
+            {
+
+                FileContent = ContentTextArea.Text;
+                var file = new FileInfo(FilePath);
+
+                try
+                {
+                    File.AppendAllText(file.FullName, FileContent);
+                    ErrorPermission.Text = "";
+                }
+                catch (Exception exception)
+                {
+                    Debug.WriteLine(exception);
+                }
+            }
+            else
+            {
+                ErrorPermission.Text = "Required permission to append data to file!";
+            }
+        }
+
+        protected void RadioPermissionChange_CheckedChanged(object sender, EventArgs e)
+        {
+            FilePath = FilePathId.Text;
+            if (FilePath == "")
+            {
+                ErrorPermission.Text = "Required file name!";
+            }
+            else if (!File.Exists(FilePath))
+            {
+                ErrorPermission.Text = "File is not yet created!";
+            }
+            else
+            {
+                ErrorPermission.Text = "";
+                try
+                {
+                    switch (PermissionId.Text)
+                    {
+                        case "1":
+                            ContentTextArea.Text = File.ReadAllText(FilePath);
+                            ContentTextArea.ReadOnly = true;
+                            break;
+                        case "2":
+                            ContentTextArea.Text = File.ReadAllText(FilePath);
+                            ContentTextArea.ReadOnly = false;
+                            break;
+                        case "3":
+                            ContentTextArea.ReadOnly = false;
+                            break;
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Debug.WriteLine(exception);
+                }
+            }
+        }
+
+        protected void DropDownPermissionChange_CheckedChanged(object sender, EventArgs e)
+        {
+            FilePath = FilePathId.Text;
+
+            if (FilePath == "")
+            {
+                ErrorPermission.Text = "Required file name!";
+            }
+            else if (!File.Exists(FilePath))
+            {
+                ErrorPermission.Text = "File is not yet created!";
+            }
+            else
+            {
+                ErrorPermission.Text = "";
+                try
+                {
+                    PermissionId.Visible = false;
+                    switch (ChangePermissionId.Text)
+                    {
+                        case "1":
+                            ContentTextArea.Text = File.ReadAllText(FilePath);
+                            ContentTextArea.ReadOnly = true;
+                            break;
+                        case "2":
+                            ContentTextArea.Text = File.ReadAllText(FilePath);
+                            ContentTextArea.ReadOnly = false;
+                            break;
+                        case "3":
+                            ContentTextArea.ReadOnly = false;
+                            break;
+                            default:
+                            PermissionId.Visible = true;
+                            break;
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Debug.WriteLine(exception);
+                }
+            }
+        }
+    }
+}
